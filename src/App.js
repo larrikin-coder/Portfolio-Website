13ddb95327c1bca4c4bef802e6f6e7b3feb4303c
larrikin-coder <shauryat.22703@gmail.com>
2025-05-29 02:21:41 +0530

    loading screen pacman

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
+import { useState,useEffect} from 'react';
 import './App.css';
-
+import { PacmanLoader } from 'react-spinners';
 function App() {
+  let [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+      <PacmanLoader/>
+      </div>
+    );
+  }
   return (
     <div className="App">
+    Welcome
     </div>
   );
 }
