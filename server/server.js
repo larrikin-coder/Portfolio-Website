bbbb0781c029e9735c41068c26b99393f8890d68
larrikin-coder <shauryat.22703@gmail.com>
2025-06-20 23:30:53 +0530

    two ports listening closed one

--- server/server.js
+++ server/server.js
@@ -172,4 +172,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
